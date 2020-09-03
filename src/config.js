d50731d24178efa0fddbfa947491d6702ffe2382
John <jpending@gmail.com>
2020-09-03 18:16:15 -0400

    fixed endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8800/api/bookmarks`,
+  API_ENDPOINT: `https://fierce-temple-27335.herokuapp.com/api/bookmarks`,
   API_KEY: "3a2f1db6-ea47-4585-bb93-c5cf47684401",
 }
